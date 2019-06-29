b8b645cdb1ae5897d2c2f636e7ba04fb2acda11d
Natdenice <clad1987@hotmail.com>
2019-06-29 11:34:50 +0200

    Added Bcrypt in order to Hash Password to increase security of our database, hashed password of users implemanted with Bcrypt in users route

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@
 /* eslint-disable no-unused-vars */
 const express = require('express');
 const mongoose = require('mongoose');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const { validate, User } = require('../models/user');
 
 const router = express.Router();
@@ -23,15 +25,37 @@ router.post('/', async (req, res) => {
 
   // if not registered, allow to create a new user
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
+  // user = new User({
+  //  name: req.body.name,
+  //  email: req.body.email,
+  //  password: req.body.password
+  // });
 
-  // save user in our DB
+  // Lodash Way more dry
+
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  // We always need to hash the password with Bcrypt, its async code too
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  // save user in our DB with properly hashed password
   user = await user.save();
-  res.status(200).send(user);
+
+  // 2 methods to control our response to the client :
+
+  // res.status(200).send({
+  //  name: user.name,
+  //  email: user.email
+  // });
+
+  // this way we can exclude the password and the version properties
+
+  // Or we can use Lodash which allows us many functionalities to manage objects
+  // we use the pick method related to lodash , and then ( inside an array )
+  // as first argument, we select
+  // the object that we want to work with, and as second , third etc arguments, the properties
+  // selected => so we will get a new object with theses 3 properties (_id, email and name)
+
+  res.status(200).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 // get all users
